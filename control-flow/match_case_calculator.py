f0988058988e6baa993dec50663cff2579281646
all-in-block <web3.allin@gmail.com>
2024-06-26 16:06:44 +0100

    another git

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1 = input("Enter the first number:")
 num2 = input("Enter the second number:")
-operator = input("Choose the operation (+, -, *, /):")
-match operator:
+operation = input("Choose the operation (+, -, *, /):")
+match operation:
     case "+":
         print("The result is" , num1 + num2 ,".")
     case "-":
@@ -9,7 +9,7 @@ match operator:
     case "*":
         print("The result is" , num1 * num2 ,".")
     case "/":
-        if num2 ==0:
+        if num2 == 0:
             print("Cannot divide by zero.")
         else:
             print("The result is" , num1 / num2 ,".")
