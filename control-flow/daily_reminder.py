d4d7bfc0462977b97a4ca002c4b6e2c665d08f57
all-in-block <web3.allin@gmail.com>
2024-06-29 20:26:14 +0100

    another add

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,20 @@
-task =input("enter a task description ")
-priority = input("enter the task’s priority (high, medium, low) ")
-time_bound = input("is the task time bound (yes or no)? ")
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
 match priority:
-    case "high":
-        if time_bound == "yes":
-            print("reminder: ", task,"is a ",priority,"priority task that requires immediate attention today!")
-        else:
-            print("reminder: ", task,"is a ",priority, "priority")
-    case "medium":
-        if time_bound == "yes":
-            print("reminder: ", task,"is a ",priority,"priority task that requires immediate attention today!")
-        else:
-            print("reminder: ", task,"is a ",priority, "priority")
-    case "low":
-        if time_bound == "yes":
-            print("reminder: ", task,"is a ",priority,"priority task that requires immediate attention today!")
-        else:
-            print("reminder: ", task,"is a ",priority, "priority")
-    case _:
-        print("enter a valid priority")
+    case 'high':
+        if time_bound == 'yes':
+            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        elif time_bound =='no':
+            print(f"Note: {task} is a {priority} priority task! Plan to do it ASAP!")
+    case 'medium':
+        if time_bound == 'yes':
+            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        elif time_bound =='no':
+            print(f"Note: {task} is a {priority} priority task! Ensure to do it before the end of the week!")
+    case 'low':
+        if time_bound == 'yes':
+            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        elif time_bound =='no':
+            print(f"Note: {task} is a {priority} priority task! Make sure you remember to do it!")
