9276f1b5123da7164098aecdca69b1d451b200c7
all-in-block <web3.allin@gmail.com>
2024-07-15 10:53:23 +0100

    yes add

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self):
+        self.account_balance = 0
+
+    def deposit(amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print("Enter a valid amount")
+
+
+
+    
